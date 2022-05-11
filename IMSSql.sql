ed42d43ac7bf44b34ebed1488a2bffe40bca63ce
Hassan17360 <l191184@lhr.nu.edu.pk>
2022-05-11 15:38:18 +0500

    S1

--- IMSSql.sql
+++ IMSSql.sql
@@ -1,6 +1,6 @@
 create database IMS
 use IMS
-
+drop table Products
 select* from Products
 
 insert into Products values(100, 'Screw', '001', '001', 10, 14);
@@ -10,7 +10,7 @@ create Table Products(
 	Product_Name varchar(50) NOT NULL,
 	Product_Description varchar(50),
 	Product_Category varchar(50),
-	Product_Quantity varchar(50),
+	Product_Quantity int,
 	Product_Per_Unit_Price float);
 
 create Table Supplier(
