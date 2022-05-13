b261b2637abd433d6afd0158e6e10ee7738fbf2c
arslantariq1110 <l191110@lhr.nu.edu.pk>
2022-05-13 07:04:28 +0500

    1.4 Sql Update

--- IMSSql.sql
+++ IMSSql.sql
@@ -2,18 +2,16 @@ create database IMS
 use IMS
 
 select* from Products
-
 insert into Products values(100, 'Screw', '001', '001', 10, 14);
-
-insert into Products values(110, 'Plug', '002', '001', 12, 14);
-insert into Products values(101, 'PTO Finger Set', '002', 'KPR', 12, 850);
-insert into Products values(122, 'R1 4-litre Can', '002', 'Oil-R1', 12, 2125)
-
-insert into Products values(152, 'R1 5 litre Can', '002', 'Oil R2', 12, 2125)
-
-update Products set Product_Name = 'Garari', Product_Quantity=2001 where id=129;
-
-Delete from Products where id=129
+insert into Products values(101, 'Screw1', '002', '001', 10, 14.5);
+insert into Products values(102, 'Screw2', '003', '001', 10, 14.7);
+insert into Products values(103, 'Screw3', '004', '001', 10, 14.8);
+insert into Products values(104, 'Screw4', '005', '001', 10, 14.3);
+insert into Products values(105, 'Screw5', '006', '001', 10, 14.4);
+insert into Products values(106, 'Screw6', '007', '001', 10, 14.9);
+insert into Products values(107, 'Screw7', '008', '001', 10, 17);
+insert into Products values(108, 'Screw8', '009', '001', 10, 19);
+insert into Products values(109, 'Screw9', '010', '001', 10, 12);
 
 create Table Products(
 	ID int NOT NULL PRIMARY KEY,
@@ -23,6 +21,13 @@ create Table Products(
 	Product_Quantity int,
 	Product_Per_Unit_Price float);
 
+select * from Supplier
+insert into Supplier values(1, 'Supplier1', '03310010101', 'supplier@gmail.com', 'Faisal Town');
+insert into Supplier values(2, 'Supplier2', '03310010101', 'supplier2@gmail.com', 'Faisal Town');
+insert into Supplier values(3, 'Supplier3', '03310010101', 'supplier3@gmail.com', 'Faisal Town');
+insert into Supplier values(4, 'Supplier4', '03310010101', 'supplier4@gmail.com', 'Faisal Town');
+insert into Supplier values(5, 'Supplier5', '03310010101', 'supplier5@gmail.com', 'Faisal Town');
+
 create Table Supplier(
 	ID int NOT NULL PRIMARY KEY,
 	Supplier_Name varchar(50),
@@ -30,13 +35,24 @@ create Table Supplier(
 	Supplier_Email varchar(50),
 	Supplier_Address varchar(50));
 
+select* from SupplyingOrder
+insert into SupplyingOrder values(1, 1, getdate()-1);
+insert into SupplyingOrder values(1, 2, getdate()-1);
+insert into SupplyingOrder values(1, 3, getdate()-1);
+insert into SupplyingOrder values(1, 4, getdate()-1);
+
 create Table SupplyingOrder(
 	Supplier_ID int NOT NULL,
 	Order_ID int NOT NULL PRIMARY KEY,
-	Supplying_Date Date
+	Supplying_Date Date,
+	Total_Bill
 	FOREIGN KEY (Supplier_ID) REFERENCES Supplier(ID));
 
-
+select * from SupplyOrderDetails
+insert into SupplyOrderDetails values (1,101);
+insert into SupplyOrderDetails values (2,101);
+insert into SupplyOrderDetails values (3,102);
+insert into SupplyOrderDetails values (4,103);
 
 create Table SupplyOrderDetails(
 	Supplying_Order_ID int NOT NULL,
@@ -49,7 +65,12 @@ create Table SupplyOrderDetails(
 /*create Table Payments()*/
 
 	
-
+select * from Customer
+insert into Customer values (1, 'Customer1', '03315566778', 'cust1@gmail.com', 'Johar Town');
+insert into Customer values (2, 'Customer2', '03315566779', 'cust2@gmail.com', 'Johar Town');
+insert into Customer values (3, 'Customer3', '03315566774', 'cust3@gmail.com', 'Johar Town');
+insert into Customer values (4, 'Customer4', '03315672138', 'cust4@gmail.com', 'Johar Town');
+insert into Customer values (5, 'Customer5', '03315226778', 'cust5@gmail.com', 'Johar Town');
 create Table Customer(
 	ID int NOT NULL PRIMARY KEY,
 	Customer_Name varchar(50),
@@ -58,25 +79,11 @@ create Table Customer(
 	Customer_Address varchar(50)
 	UNIQUE(Customer_Phone_No));
 
-/*
-create Table Batch(
-	Batch_ID int NOT NULL PRIMARY KEY,
-	Supplier_ID int NOT NULL,
-	Batch_Cost float NOT NULL, 
-	[Date] Date NOT NULL,
-	FOREIGN KEY (Supplier_ID) REFERENCES Supplier(ID)); 
-
-
-create Table Stock(
-	Batch_ID int NOT NULL,
-	Product_ID int NOT NULL,
-	Purchase_Unit_Cost float NOT NULL,
-	Selling_Unit_Cost float NOT NULL,
-	Quantity int NOT NULL,
-	PRIMARY KEY (Batch_ID, Product_ID),
-	FOREIGN KEY (Batch_ID) REFERENCES Batch(Batch_ID),
-	FOREIGN KEY (Product_ID) REFERENCES Products(ID));
-*/
+select * from Sales
+insert into Sales values (1,1,getdate(), 2000, 1500, 0);
+insert into Sales values (2,4,getdate(), 4000, 1600, 0);
+insert into Sales values (3,5,getdate(), 2000, 1000, 0);
+insert into Sales values (4,4,getdate(), 2000, 1600, 0);
 
 create Table Sales(
 	Sales_ID int NOT NULL PRIMARY KEY,
@@ -84,7 +91,13 @@ create Table Sales(
 	[Date] Date NOT NULL,
 	Total_Bill float NOT NULL,
 	Paid_Bill float NOT NULL,
+	Sales_Flag int NOT NULL,
 	FOREIGN KEY (Customer_ID) REFERENCES Customer(ID));
+
+select * from SalesDetails
+insert into SalesDetails values (1,101, 100, 14);
+insert into SalesDetails values (2,101, 20, 14);
+insert into SalesDetails values (3,101, 200, 14);
 
 create Table SalesDetails(
 	Sales_ID int NOT NULL,
@@ -95,8 +108,66 @@ create Table SalesDetails(
 	FOREIGN KEY (Sales_ID) REFERENCES Sales(Sales_ID),
 	FOREIGN KEY (Product_ID) REFERENCES Products(ID));
 
-create Table Receipt(
+select * from Accured_Payments /*Accrued payments*/ 
+insert into Accured_Payments values (1, 1, 500);
+insert into Accured_Payments values (2, 4, 600);
+insert into Accured_Payments values (3, 5, 500);
+
+
+create Table Accured_Payments(
 	Receipt_ID int NOT NULL Primary Key,
 	Customer_ID int NOT NULL,
 	Paid_Price float NOT NULL,
 	FOREIGN KEY(Customer_ID) REFERENCES Customer(ID));
+
+
+/* Cutomer Details with total bill and remaining balance */
+select Paid_Bills.ID, Paid_Bills.Customer_Name, Paid_Bills.Customer_Phone_No,Paid_Bills.Customer_Email,Paid_Bills.Customer_Address, Paid_Bills.Total_Purchases  ,(Paid_Bills.Total_Purchases - (Paid_Bills.Paid_Bill+Accured_Payments_Table.Accured_Payment)) as Balance
+From
+	(select C.ID, C.Customer_Name, C.Customer_Phone_No, C.Customer_Email, C.Customer_Address, SUM(S.Total_Bill) as Total_Purchases, (Sum(S.Paid_Bill)) as Paid_Bill
+	From Customer as C left join Sales as S on C.ID=S.Customer_ID 
+	Group by C.ID, C.Customer_Name, C.Customer_Phone_No, C.Customer_Email, C.Customer_Address) as Paid_Bills left join
+
+	(select C.ID, SUm(AP.Paid_Price) as Accured_Payment
+	From Customer as C join Accured_Payments as AP on C.ID=AP.Customer_ID
+	Group by C.ID) as Accured_Payments_Table on Paid_Bills.ID=Accured_Payments_Table.ID
+
+
+CREATE PROCEDURE GetCutomerRecord
+AS
+BEGIN
+select Paid_Bills.ID, Paid_Bills.Customer_Name, Paid_Bills.Customer_Phone_No,Paid_Bills.Customer_Email,Paid_Bills.Customer_Address, Paid_Bills.Total_Purchases  ,(Paid_Bills.Total_Purchases - (Paid_Bills.Paid_Bill+Accured_Payments_Table.Accured_Payment)) as Balance
+From
+	(select C.ID, C.Customer_Name, C.Customer_Phone_No, C.Customer_Email, C.Customer_Address, SUM(S.Total_Bill) as Total_Purchases, (Sum(S.Paid_Bill)) as Paid_Bill
+	From Customer as C left join Sales as S on C.ID=S.Customer_ID 
+	Group by C.ID, C.Customer_Name, C.Customer_Phone_No, C.Customer_Email, C.Customer_Address) as Paid_Bills left join
+
+	(select C.ID, SUm(AP.Paid_Price) as Accured_Payment
+	From Customer as C join Accured_Payments as AP on C.ID=AP.Customer_ID
+	Group by C.ID) as Accured_Payments_Table on Paid_Bills.ID=Accured_Payments_Table.ID
+	Return;
+END
+
+CREATE PROCEDURE GetSearchedCutomerRecord
+@id int,
+@name varchar(50),
+@phone varchar(20)
+AS
+BEGIN
+select Paid_Bills.ID, Paid_Bills.Customer_Name, Paid_Bills.Customer_Phone_No,Paid_Bills.Customer_Email,Paid_Bills.Customer_Address, Paid_Bills.Total_Purchases  ,(Paid_Bills.Total_Purchases - (Paid_Bills.Paid_Bill+Accured_Payments_Table.Accured_Payment)) as Balance
+From
+	(select C.ID, C.Customer_Name, C.Customer_Phone_No, C.Customer_Email, C.Customer_Address, SUM(S.Total_Bill) as Total_Purchases, (Sum(S.Paid_Bill)) as Paid_Bill
+	From Customer as C left join Sales as S on C.ID=S.Customer_ID 
+	Group by C.ID, C.Customer_Name, C.Customer_Phone_No, C.Customer_Email, C.Customer_Address) as Paid_Bills left join
+
+	(select C.ID, Sum(AP.Paid_Price) as Accured_Payment
+	From Customer as C join Accured_Payments as AP on C.ID=AP.Customer_ID
+	Group by C.ID) as Accured_Payments_Table on Paid_Bills.ID=Accured_Payments_Table.ID
+Where Paid_Bills.ID=@id or Paid_Bills.Customer_Name like @name+'%' or Paid_Bills.Customer_Phone_No like @phone+'%' 
+	Return;
+END
+
+
+drop procedure GetCutomerRecord
+
+SELECT SalesDetails.*, Sales.Customer_ID FROM SalesDetails join Sales on SalesDetails.Sales_ID=Sales.Sales_ID ORDER BY Sales_ID DESC
